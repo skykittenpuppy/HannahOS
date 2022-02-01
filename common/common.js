907ad7a54d8db23000d553f8b4cc5991497f1766
Hannah <robotooth13@gmail.com>
2022-02-01 19:40:38 +0000

    Testing discord embed?

--- common/common.js
+++ common/common.js
@@ -2,16 +2,19 @@ const head = document.head;
 const body = document.body;
 
 var importItems = document.createElement("span");
-var navBar = document.createElement("nav");
-navBar.setAttribute("class", "navbar");
 importItems.innerHTML = 
 `
 <link rel="stylesheet" href="common/common.css" />
 <!--<link rel="stylesheet" href="common/font-awesome.css"/>-->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans:wght@400&amp;display=swap">
 `
+head.appendChild(importItems);
+
+var navBar = document.createElement("nav");
+navBar.setAttribute("class", "navbar");
 navBar.innerHTML =
-`<ul class="navbar-nav">
+`
+<ul class="navbar-nav">
     <li class ="nav-item"><a href = "home">
         <svg class="icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="home" class="svg-inline--fa fa-home fa-w-18" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512"><path fill="currentColor" d="M280.37 148.26L96 300.11V464a16 16 0 0 0 16 16l112.06-.29a16 16 0 0 0 15.92-16V368a16 16 0 0 1 16-16h64a16 16 0 0 1 16 16v95.64a16 16 0 0 0 16 16.05L464 480a16 16 0 0 0 16-16V300L295.67 148.26a12.19 12.19 0 0 0-15.3 0zM571.6 251.47L488 182.56V44.05a12 12 0 0 0-12-12h-56a12 12 0 0 0-12 12v72.61L318.47 43a48 48 0 0 0-61 0L4.34 251.47a12 12 0 0 0-1.6 16.9l25.5 31A12 12 0 0 0 45.15 301l235.22-193.74a12.19 12.19 0 0 1 15.3 0L530.9 301a12 12 0 0 0 16.9-1.6l25.5-31a12 12 0 0 0-1.7-16.93z"></path></svg>
         Home
@@ -25,24 +28,30 @@ navBar.innerHTML =
         Tutorials</a>
     </li>
     <li class ="nav-item has-dropdown">
-    <a href="javascript: return false;">
+    <a href="javascript: void(0);">
             <svg class="icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="adjust" class="svg-inline--fa fa-adjust fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M8 256c0 136.966 111.033 248 248 248s248-111.034 248-248S392.966 8 256 8 8 119.033 8 256zm248 184V72c101.705 0 184 82.311 184 184 0 101.705-82.311 184-184 184z"></path></svg>
             Theme
         </a>
         <ul class="dropdown">
             <li class="dropdown-item theme-light">
-                <a id="light" href="javascript: return false;">light</a>
+                <a id="light" href="javascript: void(0);">
+                <svg class="icon-theme" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+                Light</a>
             </li>
             <li class="dropdown-item theme-dark">
-                <a id="dark" href="javascript: return false;">dark</a>
+                <a id="dark" href="javascript: void(0);">
+                <svg class="icon-theme" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+                Dark</a>
             </li>
             <li class="dropdown-item theme-pastel">
-                <a id="pastel" href="javascript: return false;">pastel</a>
+                <a id="pastel" href="javascript: void(0);">
+                <svg class="icon-theme" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="check" class="svg-inline--fa fa-check fa-w-16" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path fill="currentColor" d="M173.898 439.404l-166.4-166.4c-9.997-9.997-9.997-26.206 0-36.204l36.203-36.204c9.997-9.998 26.207-9.998 36.204 0L192 312.69 432.095 72.596c9.997-9.997 26.207-9.997 36.204 0l36.203 36.204c9.997 9.997 9.997 26.206 0 36.204l-294.4 294.401c-9.998 9.997-26.207 9.997-36.204-.001z"></path></svg>
+                Pastel</a>
             </li>
         </ul>
     </li>
     <li class ="nav-item has-dropdown">
-        <a href="javascript: return false;">
+        <a href="javascript: void(0);">
         <svg class="icon" aria-hidden="true" focusable="false" data-prefix="fas" data-icon="users" class="svg-inline--fa fa-users fa-w-20" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 640 512"><path fill="currentColor" d="M96 224c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm448 0c35.3 0 64-28.7 64-64s-28.7-64-64-64-64 28.7-64 64 28.7 64 64 64zm32 32h-64c-17.6 0-33.5 7.1-45.1 18.6 40.3 22.1 68.9 62 75.1 109.4h66c17.7 0 32-14.3 32-32v-32c0-35.3-28.7-64-64-64zm-256 0c61.9 0 112-50.1 112-112S381.9 32 320 32 208 82.1 208 144s50.1 112 112 112zm76.8 32h-8.3c-20.8 10-43.9 16-68.5 16s-47.6-6-68.5-16h-8.3C179.6 288 128 339.6 128 403.2V432c0 26.5 21.5 48 48 48h288c26.5 0 48-21.5 48-48v-28.8c0-63.6-51.6-115.2-115.2-115.2zm-223.7-13.4C161.5 263.1 145.6 256 128 256H64c-35.3 0-64 28.7-64 64v32c0 17.7 14.3 32 32 32h65.9c6.3-47.4 34.9-87.3 75.2-109.4z"></path></svg>            Associated
         </a>
         <ul class="dropdown vertical">
@@ -67,9 +76,16 @@ navBar.innerHTML =
             </li>
         </ul>
     </li>
-</ul>`
-head.appendChild(importItems);
+</ul>
+`
 body.firstChild.before(navBar);
+var navigation = document.createElement("a");
+navigation.innerHTML = 
+`<svg class="icon" aria-hidden="true" focusable="false" data-prefix="fab" data-icon="accessible-icon" class="svg-inline--fa fa-accessible-icon fa-w-14" role="img" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path fill="currentColor" d="M423.9 255.8L411 413.1c-3.3 40.7-63.9 35.1-60.6-4.9l10-122.5-41.1 2.3c10.1 20.7 15.8 43.9 15.8 68.5 0 41.2-16.1 78.7-42.3 106.5l-39.3-39.3c57.9-63.7 13.1-167.2-74-167.2-25.9 0-49.5 9.9-67.2 26L73 243.2c22-20.7 50.1-35.1 81.4-40.2l75.3-85.7-42.6-24.8-51.6 46c-30 26.8-70.6-18.5-40.5-45.4l68-60.7c9.8-8.8 24.1-10.2 35.5-3.6 0 0 139.3 80.9 139.5 81.1 16.2 10.1 20.7 36 6.1 52.6L285.7 229l106.1-5.9c18.5-1.1 33.6 14.4 32.1 32.7zm-64.9-154c28.1 0 50.9-22.8 50.9-50.9C409.9 22.8 387.1 0 359 0c-28.1 0-50.9 22.8-50.9 50.9 0 28.1 22.8 50.9 50.9 50.9zM179.6 456.5c-80.6 0-127.4-90.6-82.7-156.1l-39.7-39.7C36.4 287 24 320.3 24 356.4c0 130.7 150.7 201.4 251.4 122.5l-39.7-39.7c-16 10.9-35.3 17.3-56.1 17.3z"></path></svg>
+Skip to Main Content`
+navigation.setAttribute("class", "skip-to-main");
+navigation.setAttribute("href", window.location.href.concat("#main-content"));
+body.firstChild.before(navigation);
 
 const darkButton = document.getElementById("dark");
 const lightButton = document.getElementById("light");
@@ -77,8 +93,11 @@ const pastelButton = document.getElementById("pastel");
 
 const theme = localStorage.getItem("theme");
 const pastel = localStorage.getItem("pastelEnabled");
-console.log(theme, pastel);
+//console.log(theme, pastel);
 if (theme) {
+    for (item of body.classList.values()){
+        body.classList.remove(item);
+    }
     body.classList.add(theme);
     if (pastel) { body.classList.add("pastel"); }
 }
